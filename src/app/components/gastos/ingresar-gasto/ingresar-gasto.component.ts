e735c540e9bba3c3bf6e18b6e752ff85e5501e61
artssoria <arts.soria9@gmail.com>
2023-01-27 09:23:09 -0300

    commit de presupuesto

---
+++ src/app/components/gastos/ingresar-gasto/ingresar-gasto.component.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+import { PresupuestoService } from 'src/app/services/presupuesto.service';
+
+@Component({
+  selector: 'app-ingresar-gasto',
+  templateUrl: './ingresar-gasto.component.html',
+  styleUrls: ['./ingresar-gasto.component.css']
+})
+export class IngresarGastoComponent {
+nombreGasto: string;
+cantidad: number;
+formularioIncorrecto:boolean;
+textIncorrecto: string;
+
+constructor(private _presupuestoService: PresupuestoService){
+  this.nombreGasto = '';
+  this.cantidad = 0;
+  this.formularioIncorrecto = false;
+  this.textIncorrecto = '';
+}
+
+agregarGasto(){
+  if(this.cantidad > this._presupuestoService.restante){
+    this.formularioIncorrecto = true;
+    this.textIncorrecto = 'Cantidad ingresada es mayor al restante';
+    return;
+  }
+
+
+  if(this.nombreGasto == '' || this.cantidad <= 0){
+    this.formularioIncorrecto = true;
+    this.textIncorrecto = 'Nombre gasto o cantidad incorrecta';
+  }else{
+
+    //crear el objeto
+const GASTO = {
+  nombre:this.nombreGasto,
+  cantidad:this.cantidad
+}
+
+    //enviar el obj a los subs via subjet
+
+
+    //reset form
+    this.formularioIncorrecto = false;
+    this.nombreGasto = '';
+    this.cantidad = 0;
+  }
+}
+
+
+
+}
